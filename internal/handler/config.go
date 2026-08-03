72180dc5c1a6012787360f0fcaf0d7562c1f6933
abdullahbasir <abdullah.sbasir@gmail.com>
2026-08-03 18:20:24 +0400

    User login added, aswell as jwt helper functions

--- internal/handler/config.go
+++ internal/handler/config.go
@@ -9,10 +9,12 @@ import (
 	"github.com/AbdullahBasir/financial-tracker/database/sqlc"
 	"github.com/google/uuid"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	dbQueries *sqlc.Queries
+	jwtSecret string
 }
 
 type User struct {
@@ -30,6 +32,11 @@ func LoadConfig() apiConfig {
 		log.Fatal("DB_URL must be set")
 	}
 
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if dbUrl == "" {
+		log.Fatal("JWT_SECRET must be set")
+	}
+
 	db, err := sql.Open("postgres", dbUrl)
 	if err != nil {
 		log.Fatalf("could not open database: %v", err)
@@ -39,5 +46,6 @@ func LoadConfig() apiConfig {
 
 	return apiConfig{
 		dbQueries: dbQueries,
+		jwtSecret: jwtSecret,
 	}
 }
